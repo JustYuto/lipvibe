cebcbaabd72a47a5a56315af69f96e104737b148
SebChia <104203508+SebChia@users.noreply.github.com>
2023-03-04 13:46:17 +0800

    Update flexIS.sql

--- MySQL/flexIS.sql
+++ MySQL/flexIS.sql
@@ -1,11 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `flexIS`;
 USE `flexIS`;
 
-
-DROP TABLE EMPLOYEE;
-DROP TABLE DEPARTMENT;
-DROP TABLE FWA_Rquest;
-
 CREATE TABLE EMPLOYEE(     
     employeeID VARCHAR(5) NOT NULL, 
     password VARCHAR(30) NOT NULL, 
@@ -13,8 +8,8 @@ CREATE TABLE EMPLOYEE(
     email VARCHAR (30) NOT NULL, 
     position VARCHAR (20) NOT NULL, 
     FWAStatus VARCHAR (15) NOT NULL,  
-    departmentID VARCHAR(5) NOT NULL, 
-    supervisorID VARCHAR(5), 
+    departmentID CHAR(5) NOT NULL, 
+    supervisorID CHAR(5), 
     CONSTRAINT EMP_PK PRIMARY KEY (employeeID), 
     CONSTRAINT EMP_FK1 FOREIGN KEY (supervisorID) REFERENCES EMPLOYEE(employeeID) on delete cascade
 ); 
@@ -23,30 +18,26 @@ CREATE TABLE DEPARTMENT(
     departmentID VARCHAR(5) NOT NULL, 
     deptName VARCHAR(20) NOT NULL, 
     employeeID VARCHAR(5) NOT NULL,
-    CONSTRAINT DEPARTMENT_PK PRIMARY KEY (departmentID), 
-    CONSTRAINT FWA_Rqu_FK FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) on delete cascade
+    CONSTRAINT DEPARTMENT_PK PRIMARY KEY (departmentID)
 ); 
 
 CREATE TABLE FWA_Rquest(     
     requestID VARCHAR(5) not null,     
     requestDate DATE not null,     
-    workType VARCHAR(30) not null, 
+    workType VARCHAR(30) not null CHECK (workType IN('Flexi-hour', 'Work-from-home','Hybrid')), 
     description VARCHAR(255) not null, 
     reason VARCHAR(255) not null, 
-    status VARCHAR(20) not null, 
+    status VARCHAR(20) not null CHECK (status IN('Failed','Accepted','Progress')), 
     comment VARCHAR(255), 
     employeeID VARCHAR(5) not null, 
-    CONSTRAINT fwa_Rqu_PK2 PRIMARY KEY (requestID), 
+    CONSTRAINT fwa_Rqu_PK2 PRIMARY KEY (requestID)
 ); 
 
-ALTER TABLE EMPLOYEE
-  ADD CONSTRAINT EMP_FK2 FOREIGN KEY (departmentID) REFERENCES DEPARTMENT (departmentID) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE EMPLOYEE ADD 
+	CONSTRAINT EMP_FK2 FOREIGN KEY (departmentID) REFERENCES DEPARTMENT (departmentID) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE DEPARTMENT
-  ADD CONSTRAINT Dp_FK FOREIGN KEY (employeeID) REFERENCES EMPLOYEE (employeeID) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE DEPARTMENT ADD 
+	CONSTRAINT Dp_FK FOREIGN KEY (employeeID) REFERENCES EMPLOYEE (employeeID) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE FWA_Rquest
-    ADD CONSTRAINT FWA_Rqu_CHK1 check (workType = 'Flexi-hour' or workType = 'Work-from-home', or workType = 'Hybrid'),
-    CONSTRAINT FWA_Rqu_FK FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) on delete cascade, 
-    CONSTRAINT FWA_Rqu_CHK2 check (status ='Failed' or status ='Accepted' or status='Progress')
-;
+ALTER TABLE FWA_Rquest ADD
+    CONSTRAINT FWA_Rqu_FK FOREIGN KEY (employeeID) REFERENCES Employee(employeeID) on delete cascade;
